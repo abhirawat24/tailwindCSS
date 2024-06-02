c21aefd660f0204338ad54f2f1f7257ef40a9826
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2024-06-02 18:44:29 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { HiBars3 } from "react-icons/hi2";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center gap-2">
+        <img src="./assets/Logo.svg" alt="logo" />
+        <button className="bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl">
+          Hoster is hiring
+        </button>
+      </div>
+      <div>
+        <HiBars3 />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
